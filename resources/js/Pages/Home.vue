9bd7e891632fd8ca4ae849c2a17e6ce679e92668
Pol Lamothe <lamothepol@gmail.com>
2024-12-16 16:28:59 +0100

    Initialisation de Inertia

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>Voici le props passé : {{ test }}</p>
+    <p>{{ compteur }}</p>
+    <button @click="compteur++">Incrémenter</button>
+</template>
+
+<script setup>
+import {ref} from "vue"
+
+defineProps({
+    test:{
+        type:String
+    }
+})
+
+let compteur = ref(0)
+
+</script>
