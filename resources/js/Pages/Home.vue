d0d0e0c73b0ebae2e94b1b5a2f1299c4143f57c8
Pol Lamothe <lamothepol@gmail.com>
2024-12-17 14:44:32 +0100

    Ajout du component formulaire

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,12 +4,16 @@
     <button @click="compteur++">Incrémenter</button>
     <p v-if="urlData">Voici la valeur passé en url : {{ urlData }}</p>
     <p v-else>Vous n'avez pas passé de valeur dans l'url</p>
+    <div id="form">
+        <Form :fields="[{'name':'Adresse e-mail'},{'name':'Mot de passe',type:'password'}]" button-text="Connexion" :check-boxs="['Se souvenir de moi']" :links="[{'text':'Mot de passe oublié ?','link':'youtube.com'}]"></Form>
+    </div>
     <Footer></Footer>
 </template>
 
 <script setup>
 import {ref} from "vue"
 import Footer from "./Components/Footer.vue"
+import Form from "./Components/Form.vue";
 
 defineProps({
     test : String,
@@ -19,3 +23,13 @@ defineProps({
 let compteur = ref(0)
 
 </script>
+
+<style scoped>
+#form{
+    width: 30vw;
+    margin-left: 50%;
+    transform: translateX(-50%);
+    margin-bottom: 10vh;
+    margin-top: 10vh;
+}
+</style>
