a9fda606f7cdd5751e6827607b06d5db900bc39e
Pol Lamothe <lamothepol@gmail.com>
2024-12-18 09:58:03 +0100

    Création du header

---
+++ resources/js/Pages/Components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="globalWrapper">
+        <div v-for="page in Object.keys(allPages)" :class="{selected : currentPage == page}" @click="redirect(page)">
+            {{ page }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    defineProps(["currentPage"])
+
+    let allPages = {
+        "Accueil":"/",
+        "Magasin":"/shop",
+        "Notre histoire" : "/story",
+        "Contact":"/contact"
+    }
+
+    function redirect(page){
+        window.location = allPages[page]
+    }
+</script>
+
+<style scoped>
+    .selected{
+        background-color: black;
+        color: white;
+    }
+    #globalWrapper div:hover{
+        background-color: black;
+        color: white;
+    }
+    #globalWrapper div{
+        font-family: "Roboto", serif;
+        padding: .75vw;
+        border-radius: 50px;
+        cursor: pointer;
+        transition-duration: .5s;
+    }
+    #globalWrapper{
+        width: fit-content;
+        display: flex;
+        flex-direction: row;
+        position: sticky;
+        margin-left: 50%;
+        transform: translateX(-50%);
+        top: 5vh;
+        gap: 2.5vw;   
+        background-color: white;
+        padding: .5vw;
+        border-radius: 50px;
+    }
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,22 +1,17 @@
 <template>
+    <Header currentPage="Accueil"></Header>
     <p>Voici le props passé : {{ test }}</p>
     <p>{{ compteur }}</p>
     <button @click="compteur++">Incrémenter</button>
     <p v-if="urlData">Voici la valeur passé en url : {{ urlData }}</p>
     <p v-else>Vous n'avez pas passé de valeur dans l'url</p>
-    <pre>
-        {{usersName}}
-    </pre>
-    <div id="form">
-        <Form :fields="[{'name':'Adresse e-mail'},{'name':'Mot de passe',type:'password'}]" button-text="Connexion" :check-boxs="['Se souvenir de moi']" :links="[{'text':'Mot de passe oublié ?','link':'youtube.com'}]"></Form>
-    </div>
     <Footer></Footer>
 </template>
 
 <script setup>
 import {ref} from "vue"
 import Footer from "./Components/Footer.vue"
-import Form from "./Components/Form.vue";
+import Header from "./Components/Header.vue";
 
 defineProps({
     test : String,
