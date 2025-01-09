eb387b962a59a321c2d40741660d192e82bb6e25
Brieuc Le Carluer <brieuc.le-carluer@etu.univ-nantes.fr>
2025-01-09 23:01:14 +0100

    Update responsive carte

--- resources/js/Pages/Carte.vue
+++ resources/js/Pages/Carte.vue
@@ -1,13 +1,29 @@
 <template>
-    <Header currentPage="Carte"></Header>
-    <iframe width="100%" height="1500px" frameborder="0" allowfullscreen allow="geolocation" src="//umap.openstreetmap.fr/fr/map/laurelin_1161286?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true"></iframe>
-    <Footer></Footer> 
-  </template>
+  <Header currentPage="Carte"></Header>
+  <iframe class="responsive-iframe" frameborder="0" allowfullscreen allow="geolocation" src="//umap.openstreetmap.fr/fr/map/laurelin_1161286?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true"></iframe>
+  <Footer></Footer> 
+</template>
 
 <script setup>
-  import Footer from "./Components/Footer.vue";
-  import Header from "./Components/Header.vue";
+import Footer from "./Components/Footer.vue";
+import Header from "./Components/Header.vue";
 </script>    
 
 <style scoped>
+.responsive-iframe {
+width: 100%;
+height: 60vh;
+}
+
+@media (max-width: 768px) {
+.responsive-iframe {
+  height: 50vh; 
+}
+}
+
+@media (max-width: 480px) {
+.responsive-iframe {
+  height: 40vh; 
+}
+}
 </style>
