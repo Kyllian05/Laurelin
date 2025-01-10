63a0e2e43ce1c235f754aff2b02c9a97f2f6a97b
Brieuc Le Carluer <brieuc.le-carluer@etu.univ-nantes.fr>
2025-01-10 15:40:42 +0100

    Update Home.vue

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,36 +1,52 @@
 <template>
     <Header currentPage="Accueil"></Header>
-    <!--
-    <p v-if="prenom">Bienvenue {{ prenom }}</p>
-    <p>Voici le props passé : {{ test }}</p>
-    <p>{{ compteur }}</p>
-    <button @click="compteur++">Incrémenter</button>
-    <p v-if="urlData">Voici la valeur passé en url : {{ urlData }}</p>
-    <p v-else>Vous n'avez pas passé de valeur dans l'url</p>-->
     <div class="hero">
+        <video autoplay muted loop class="background-video">
+            <source src="/public/images/video1.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
     </div>
     <Footer></Footer>
 </template>
 
 <script setup>
-import {ref} from "vue"
+import { ref } from "vue"
 import Footer from "./Components/Footer.vue"
 import Header from "./Components/Header.vue";
 
 defineProps({
-    test : String,
-    urlData : String,
+    test: String,
+    urlData: String,
     prenom: String,
 })
 
 let compteur = ref(0)
-
 </script>
 
 <style scoped>
-    .hero {
-        width: 100vw;
-        height: 100vh;
-        background: url("/public/images/home-1.png") no-repeat center center/cover;
-    }
+html, body {
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden; /* Désactive le défilement horizontal */
+    height: 100%; /* Assurez-vous que le corps prend toute la hauteur */
+}
+
+.hero {
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+    overflow: hidden;
+}
+
+.background-video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    object-fit: cover;
+    z-index: -1;
+}
 </style>
