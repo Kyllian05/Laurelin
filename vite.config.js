d761cf2ca2d71bb0643c2e192cdc6a54eaf69aa7
Kyllian <kyllian.arnaud@etu.univ-nantes.fr>
2025-03-31 17:24:05 +0200

    publish modif

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'; // Import du plugin Vue
 
 export default defineConfig({
+    base: '/Laurelin/',
+    build: {
+        outDir: 'dist'
+    },
     plugins: [
-        laravel({
-            input: ['resources/js/app.js', 'resources/css/app.css'],
-            refresh: true,
-        }),
-        vue(), // Active le plugin Vue
+        vue()
     ],
 });
